eae60a984518ddcf4b04a38cf90ad5062117f000
VladislavSinkovskiy <113598983+VladislavSinkovskiy@users.noreply.github.com>
2026-03-22 16:11:09 +0300

    Delete build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug directory

--- build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_LR_1_FILE C:/221-331_Sinkovskiy/LR1/build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/LR_1.exe)
-set(__QT_DEPLOY_TARGET_LR_1_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_LR_1_RUNTIME_DLLS C:/Qt/6.10.2/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Cored.dll)
